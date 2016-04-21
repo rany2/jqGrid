e744c5490d69ee094bde7897cf5388207a36c63a
Dr. Oleg Kiriljuk <oleg.kiriljuk@ok-soft-gmbh.com>
2016-04-22 01:07:21 +0200

    update min/map files

--- dist/jquery.jqgrid.src.js
+++ dist/jquery.jqgrid.src.js
@@ -8,7 +8,7 @@
  * Dual licensed under the MIT and GPL licenses
  * http://www.opensource.org/licenses/mit-license.php
  * http://www.gnu.org/licenses/gpl-2.0.html
- * Date: 2016-04-18
+ * Date: 2016-04-22
  */
 //jsHint options
 /*jshint eqnull:true */
@@ -8379,6 +8379,7 @@
 												}
 											});
 										}
+										$(elem1).change();
 										jgrid.fullBoolFeedback.call($t, options1.selectFilled, "jqGridSelectFilled", {
 											elem: elem1,
 											options: options1,
--- js/grid.base.js
+++ js/grid.base.js
@@ -8,7 +8,7 @@
  * Dual licensed under the MIT and GPL licenses
  * http://www.opensource.org/licenses/mit-license.php
  * http://www.gnu.org/licenses/gpl-2.0.html
- * Date: 2016-04-18
+ * Date: 2016-04-22
  */
 //jsHint options
 /*jshint eqnull:true */
