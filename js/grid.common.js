43999b6060e6c001030ce5e69bc8404a6fcc9479
Oleg Kiriljuk <oleg.kiriljuk@ok-soft-gmbh.com>
2017-05-28 16:21:53 +0200

    allow to use callback functions as the value of boolean properties of editrules

--- js/grid.common.js
+++ js/grid.common.js
@@ -732,6 +732,10 @@
 		checkValues: function (val, iCol, customobject, nam, options) {
 			var edtrul, nm, dft, g = this, p = g.p, colModel = p.colModel, cm, isEmpty = jgrid.isEmpty,
 				editMsg = getGridRes.call($(g), "edit.msg"), ret,
+				getPropertyValue = function (propertyName) {
+					var propValue = edtrul[propertyName];
+					return $.isFunction(propValue) ? propValue.call(g, options) : propValue;
+				},
 				dateMasks = getGridRes.call($(g), "formatter.date.masks");
 			if (customobject === undefined) {
 				if (typeof iCol === "string") {
@@ -750,12 +754,12 @@
 			}
 			if (edtrul) {
 				if (!nm) { nm = p.colNames != null ? p.colNames[iCol] : cm.label; }
-				if (edtrul.required === true) {
+				if (getPropertyValue("required") === true) {
 					if (isEmpty(val)) { return [false, nm + ": " + editMsg.required, ""]; }
 				}
 				// force required
-				var rqfield = edtrul.required === false ? false : true;
-				if (edtrul.number === true) {
+				var rqfield = getPropertyValue("required") === false ? false : true;
+				if (getPropertyValue("number") === true) {
 					if (!(rqfield === false && isEmpty(val))) {
 						if (isNaN(val)) { return [false, nm + ": " + editMsg.number, ""]; }
 					}
@@ -767,20 +771,20 @@
 					if (parseFloat(val) > parseFloat(edtrul.maxValue)) { return [false, nm + ": " + editMsg.maxValue + " " + edtrul.maxValue, ""]; }
 				}
 				var filter;
-				if (edtrul.email === true) {
+				if (getPropertyValue("email") === true) {
 					if (!(rqfield === false && isEmpty(val))) {
 						// taken from $ Validate plugin
 						filter = /^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$/i;
 						if (!filter.test(val)) { return [false, nm + ": " + editMsg.email, ""]; }
 					}
 				}
-				if (edtrul.integer === true) {
+				if (getPropertyValue("integer") === true) {
 					if (!(rqfield === false && isEmpty(val))) {
 						if (isNaN(val)) { return [false, nm + ": " + editMsg.integer, ""]; }
 						if ((val % 1 !== 0) || (val.indexOf(".") !== -1)) { return [false, nm + ": " + editMsg.integer, ""]; }
 					}
 				}
-				if (edtrul.date === true) {
+				if (getPropertyValue("date") === true) {
 					if (!(rqfield === false && isEmpty(val))) {
 						if (cm.formatoptions && cm.formatoptions.newformat) {
 							dft = cm.formatoptions.newformat;
@@ -793,12 +797,12 @@
 						if (!jgrid.checkDate(dft, val)) { return [false, nm + ": " + editMsg.date + " - " + dft, ""]; }
 					}
 				}
-				if (edtrul.time === true) {
+				if (getPropertyValue("time") === true) {
 					if (!(rqfield === false && isEmpty(val))) {
 						if (!jgrid.checkTime(val)) { return [false, nm + ": " + editMsg.date + " - hh:mm (am/pm)", ""]; }
 					}
 				}
-				if (edtrul.url === true) {
+				if (getPropertyValue("url") === true) {
 					if (!(rqfield === false && isEmpty(val))) {
 						filter = /^(((https?)|(ftp)):\/\/([\-\w]+\.)+\w{2,3}(\/[%\-\w]+(\.\w{2,})?)*(([\w\-\.\?\\\/+@&#;`~=%!]*)(\.\w{2,})?)*\/?)/i;
 						if (!filter.test(val)) { return [false, nm + ": " + editMsg.url, ""]; }
