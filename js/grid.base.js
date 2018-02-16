bd269813abb3a856ba64844f45e3c1f758aa092a
Oleg Kiriljuk <oleg.kiriljuk@ok-soft-gmbh.com>
2018-02-16 18:38:12 +0100

    prepare publishing of free jqGrid 4.15.3

--- js/grid.base.js
+++ js/grid.base.js
@@ -2,13 +2,13 @@
 // @compilation_level SIMPLE_OPTIMIZATIONS
 
 /**
- * @license jqGrid 4.15.3-pre - free jqGrid: https://github.com/free-jqgrid/jqGrid
+ * @license jqGrid 4.15.3 - free jqGrid: https://github.com/free-jqgrid/jqGrid
  * Copyright (c) 2008-2014, Tony Tomov, tony@trirand.com
- * Copyright (c) 2014-2017, Oleg Kiriljuk, oleg.kiriljuk@ok-soft-gmbh.com
+ * Copyright (c) 2014-2018, Oleg Kiriljuk, oleg.kiriljuk@ok-soft-gmbh.com
  * Dual licensed under the MIT and GPL licenses
  * http://www.opensource.org/licenses/mit-license.php
  * http://www.gnu.org/licenses/gpl-2.0.html
- * Date: 2017-01-25
+ * Date: 2018-02-16
  */
 //jsHint options
 /*jshint eqnull:true */
@@ -381,7 +381,7 @@
 
 	$.extend(true, jgrid, {
 		/** @const */
-		version: "4.15.3-pre",
+		version: "4.15.3",
 		/** @const */
 		productName: "free jqGrid",
 		defaults: {},
