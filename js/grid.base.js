54b122b8e4e89590e002eef8a1af0ef82813785a
Oleg Kiriljuk <oleg.kiriljuk@ok-soft-gmbh.com>
2018-02-24 12:36:19 +0100

    increase version of free-jqgrid from 4.15.3 to preliminary 4.15.4 (4.15.4-pre)

--- js/grid.base.js
+++ js/grid.base.js
@@ -2,13 +2,13 @@
 // @compilation_level SIMPLE_OPTIMIZATIONS
 
 /**
- * @license jqGrid 4.15.3 - free jqGrid: https://github.com/free-jqgrid/jqGrid
+ * @license jqGrid 4.15.4-pre - free jqGrid: https://github.com/free-jqgrid/jqGrid
  * Copyright (c) 2008-2014, Tony Tomov, tony@trirand.com
  * Copyright (c) 2014-2018, Oleg Kiriljuk, oleg.kiriljuk@ok-soft-gmbh.com
  * Dual licensed under the MIT and GPL licenses
  * http://www.opensource.org/licenses/mit-license.php
  * http://www.gnu.org/licenses/gpl-2.0.html
- * Date: 2018-02-16
+ * Date: 2018-02-22
  */
 //jsHint options
 /*jshint eqnull:true */
@@ -381,7 +381,7 @@
 
 	$.extend(true, jgrid, {
 		/** @const */
-		version: "4.15.3",
+		version: "4.15.4-pre",
 		/** @const */
 		productName: "free jqGrid",
 		defaults: {},
