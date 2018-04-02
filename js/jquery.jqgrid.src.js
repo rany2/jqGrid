c3e6f33bf7b9da3dac797ef0b5539ba1c1e02785
Oleg Kiriljuk <oleg.kiriljuk@ok-soft-gmbh.com>
2018-04-02 20:56:29 +0200

    rebuild all files using the latest version of uglify-js 3.3.18

--- js/jquery.jqgrid.src.js
+++ js/jquery.jqgrid.src.js
@@ -2,13 +2,13 @@
 // @compilation_level SIMPLE_OPTIMIZATIONS
 
 /**
- * @license jqGrid 4.15.4-pre - free jqGrid: https://github.com/free-jqgrid/jqGrid
+ * @license jqGrid 4.15.4 - free jqGrid: https://github.com/free-jqgrid/jqGrid
  * Copyright (c) 2008-2014, Tony Tomov, tony@trirand.com
  * Copyright (c) 2014-2018, Oleg Kiriljuk, oleg.kiriljuk@ok-soft-gmbh.com
  * Dual licensed under the MIT and GPL licenses
  * http://www.opensource.org/licenses/mit-license.php
  * http://www.gnu.org/licenses/gpl-2.0.html
- * Date: 2018-03-31
+ * Date: 2018-04-02
  */
 //jsHint options
 /*jshint eqnull:true */
@@ -381,7 +381,7 @@
 
 	$.extend(true, jgrid, {
 		/** @const */
-		version: "4.15.4-pre",
+		version: "4.15.4",
 		/** @const */
 		productName: "free jqGrid",
 		defaults: {},
